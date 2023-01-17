5d17c02a15656ce323c833adf325363d4400ff43
herdimasanggara <47119995+herdimasanggara@users.noreply.github.com>
2023-01-17 13:15:49 -0500

    Add files via upload

---
+++ dialogues.js
@@ -0,0 +1,14 @@
+$(function(){
+
+$('.accordionDrop').hide();
+              $('.accordion').on('click', function(){
+			$(this).next().slideToggle("fast");
+		})
+
+$('.accordionDrop').hide();
+              $('.accordionHighlight').on('click', function(){
+			$(this).next().slideToggle("fast");
+		})
+
+
+})
